ce458a9081e2f3926f706eed54ffb2a09a2c3646
Jacob Vosburgh <jacob_vosburgh@yahoo.com>
2017-09-06 14:17:07 +0800

    Aded a line to the title comment.

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -3,6 +3,7 @@ package seedu.addressbook;
 /*
  * Code provided by CS2103, updated by Jacob Vosburgh
  * It serves as a demonstration of baseline understanding of github functionality.
+ * Here is a better line that contains one long word, extraterrestrial.
  * NOTE : =============================================================
  * This class is written in a procedural fashion (i.e. not Object-Oriented)
  * Yes, it is possible to write non-OO code using an OO language.
